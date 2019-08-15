60c1ea2b467815895a7e41f2bb52bff3749aa219
daRm <darmanex@gmail.com>
2019-08-15 11:36:35 +0700

    update some params

--- nginx/bin/nginx-start.sh
+++ nginx/bin/nginx-start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#https://www.digitalocean.com/community/tutorials/how-to-run-nginx-in-a-docker-container-on-ubuntu-14-04
 
 docker run --name mywebserver -p 80:80 \
 	-v ~/Desktop/docker/nginx/html:/usr/share/nginx/html \
 	-v ~/Desktop/docker/nginx/html/darmsec.net:/var/www/darmsec.net/html \
 	-v ~/Desktop/docker/nginx/html/api.darmsec.net:/var/www/api.darmsec.net/html \
 	-v ~/Desktop/docker/nginx/html/grafana.darmsec.net:/var/www/grafana.darmsec.net/html \
-	-v ~/Desktop/docker/nginx/conf/darm.conf:/etc/nginx/conf.d/darm.conf \
-	-v ~/Desktop/docker/nginx/conf/api.conf:/etc/nginx/conf.d/api.conf \
-        -v ~/Desktop/docker/nginx/conf/grafana.conf:/etc/nginx/conf.d/grafana.conf \
-	-d mywebserver
+	-v ~/Desktop/docker/nginx/conf/darm.conf:/etc/nginx/conf.d/darm.conf:ro \
+	-v ~/Desktop/docker/nginx/conf/api.conf:/etc/nginx/conf.d/api.conf:ro \
+        -v ~/Desktop/docker/nginx/conf/grafana.conf:/etc/nginx/conf.d/grafana.conf:ro \
+	-d nginx/buster
